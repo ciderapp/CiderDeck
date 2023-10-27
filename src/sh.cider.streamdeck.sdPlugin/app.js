fd94b5837b2dfbc6e999da6a5f51456113783a4d
cryptofyre <cryptofyre@cryptofyre.org>
2023-10-27 06:37:18 -0500

    Fix various issues, and complete websockets.

--- src/sh.cider.streamdeck.sdPlugin/app.js
+++ src/sh.cider.streamdeck.sdPlugin/app.js
@@ -9,17 +9,18 @@ const songNameAction = new Action('sh.cider.streamdeck.songname');
 const albumArtAction = new Action('sh.cider.streamdeck.albumart');
 const likeAction = new Action('sh.cider.streamdeck.like');
 const dislikeAction = new Action('sh.cider.streamdeck.dislike');
-const addToLibraryAction = new Action('sh.cider.streamdeck.addToLibrary');
-const volumeUpAction = new Action('sh.cider.streamdeck.volumeUp');
-const volumeDownAction = new Action('sh.cider.streamdeck.volumeDown');
+const addToLibraryAction = new Action('sh.cider.streamdeck.addtolibrary');
+const volumeUpAction = new Action('sh.cider.streamdeck.volumeup');
+const volumeDownAction = new Action('sh.cider.streamdeck.volumedown');
 
 /**
  * The first event fired when Stream Deck starts
  */
 $SD.onConnected(() => {
 	console.debug('[DEBUG] [Init] Stream Deck connected!');
+	console.debug("[DEBUG] [Contexts] Available Contexts:", window.contexts);
 	setDefaults();
-	setTimeout(startWebSocket(), 500);
+	setTimeout(startWebSocket, 500);
 });
 
 // Initalize WebSocket Connection with Cider.
@@ -28,18 +29,21 @@ function startWebSocket() {
 	try {
 		// Connect to the websocket server
 		console.debug('[DEBUG] [Init] Configuring websocket connection...');
-		const CiderApp = new WebSocket('ws://localhost:10766');
+		const CiderApp = new WebSocket('ws://localhost:10766/ws');
 		console.debug('[DEBUG] [Init] Websocket connection established!');
 
 		// Set up websocket artwork/information handling
 
-		CiderApp.onMessage = (event) => {
-			console.debug('[DEBUG] [Init] Websocket message received:', event.data);
-
+		CiderApp.addEventListener("message", (event) => {
 			if (event.data !== undefined || event.data !== null) {
 				try {
 					let playbackInfo = JSON.parse(event.data);
-					setData(playbackInfo);
+
+					if (playbackInfo.data?.status !== undefined || playbackInfo.data?.artwork?.url !== undefined || playbackInfo.data?.name !== undefined) {
+						setData(playbackInfo);
+					} else {
+						console.debug("[DEBUG] [Init] PlaybackInfo is undefined or null, skipping to avoid errors.")
+					}
 				} catch (error) {
 					console.debug('[DEBUG] [Init] Websocket parsing error:', error);
 				}
@@ -47,21 +51,21 @@ function startWebSocket() {
 				console.log("[DEBUG] [Init] Websocket message is undefined or null, skipping.")
 				setDefaults();
 			}
-		}
+		});
 
-		CiderApp.onClose = () => {
+		CiderApp.addEventListener("close", (event) => {
 			console.debug('[DEBUG] [Init] Websocket connection closed!');
 			setDefaults();
 			console.debug("[DEBUG] [Init] Retrying in 5 seconds...")
 			setTimeout(startWebSocket, 5000);
-		}
+		});
 
-		CiderApp.onError = (error) => {
+		CiderApp.addEventListener("error", (event) => {
 			console.debug('[DEBUG] [Init] Websocket error:', error);
 			setDefaults();
 			console.debug("[DEBUG] [Init] Retrying in 5 seconds...")
 			setTimeout(startWebSocket, 5000);
-		}
+		});
 
 	} catch (error) {
 		console.debug('[DEBUG] [Init] Websocket error:', error);
@@ -75,131 +79,137 @@ function startWebSocket() {
 async function setDefaults () {
 	console.debug("[DEBUG] [Actions] Setting default state.")
 	// Set defaults.
-    forEach(window.contexts.toggleAction, function (context) {
+    window.contexts.toggleAction.forEach(function (context) {
 	    setImage(context, 'actions/playback/assets/play.png', 0);
     });
 
-    forEach(window.contexts.albumArtAction, function (context) {
+    window.contexts.albumArtAction.forEach(function (context) {
 		setImage(context, 'actions/playback/assets/icon.png', 0);
     });
         
-    forEach(window.contexts.songNameAction, function (context) {
+    window.contexts.songNameAction.forEach(function (context) {
 		setTitle(context, "No Song Playing", 0)
     });
 
-	forEach(window.contexts.dislikeAction, function (context) {
+	window.contexts.dislikeAction.forEach(function (context) {
 		setImage(context, 'actions/playback/assets/dislike.png', 0);
 	});
 
-	forEach(window.contexts.likeAction, function (context) {
+	window.contexts.likeAction.forEach(function (context) {
 		setImage(context, 'actions/playback/assets/like.png', 0);
 	});
 
-	forEach(window.contexts.addToLibraryAction, function (context) {
+	window.contexts.addToLibraryAction.forEach(function (context) {
 		setImage(context, 'actions/playback/assets/add.png', 0);
 	});
 
-	forEach(window.contexts.volumeUpAction, function (context) {
+	window.contexts.volumeUpAction.forEach(function (context) {
 		setImage(context, 'actions/playback/assets/volumeup.png', 0);
 	});
 
-	forEach(window.contexts.volumeDownAction, function (context) {
+	window.contexts.volumeDownAction.forEach(function (context) {
 		setImage(context, 'actions/playback/assets/volumedown.png', 0);
 	});
 }
 
 // Album Art & Song Name event handling
-
+// TODO: REMOVE WITH WEBSOCKETS
 async function setData(playbackInfo) {
-	setPlaybackStatus(playbackInfo);
+	setPlaybackStatus(playbackInfo.data?.status);
+
 	// Set variables for artwork and song name
-	window.artwork = playbackInfo.info?.artwork?.url?.replace('{w}', '100').replace('{h}', '100');
+	window.artwork = playbackInfo.data?.artwork?.url?.replace('{w}', '100').replace('{h}', '100');
 	window.artworkCache;
 	window.songCache;
+	window.kind = playbackInfo.data?.playParams?.kind;
+	window.id = playbackInfo.data?.playParams?.id;
 
 	// Cache artwork and song name to prevent unnecessary updates
 	if (window.artworkCache !== window.artwork) {
 		window.artworkCache = window.artwork;
 		console.debug("[DEBUG] [Artwork] Artwork is different, updating.")
 		let art64 = await getBase64Image(artwork);
-        forEach(window.contexts.albumArtAction, function (context) {
+        window.contexts.albumArtAction.forEach(function (context) {
 		    setImage(context, art64, 0);
         });
 	}
-	if (window.songCache !== playbackInfo.info?.name) {
-		window.songCache = playbackInfo.info?.name;
+	if (window.songCache !== playbackInfo.data?.name) {
+		window.songCache = playbackInfo.data?.name;
 		console.debug("[DEBUG] [SongName] Song is different, updating.")
 		window.addedToLibrary = false;
-		forEach(window.contexts.addToLibraryAction, function (context) {
+		window.contexts.addToLibraryAction.forEach(function (context) {
 			setImage(context, 'actions/playback/assets/add.png', 0);
 		});
-        forEach(window.contexts.songNameAction, function (context) {
-		    setTitle(context, playbackInfo.info?.name, 0)
+        window.contexts.songNameAction.forEach(function (context) {
+		    setTitle(context, playbackInfo.data?.name, 0)
         });
-	}
-	// Set Action to use the correct icon
-	if (playbackInfo.info?.status === "playing") {
-        forEach(window.contexts.toggleAction, function (context) {
-		    setImage(context, 'actions/playback/assets/pause.png', 0);
-        })
-	}
-	else if (playbackInfo.info?.status === "paused") {
-        forEach(window.contexts.toggleAction, function (context) {
-		    setImage(context, 'actions/playback/assets/play.png', 0);
-        });
-	}
 
-	// Set like/dislike icons depending on current song rating.
-	try {
-		comRPC("GET", `rating/${playbackInfo.info?.playParams?.kind}/${playbackInfo.info?.songId}`).then((ratingData) => {
-			window.ratingCache = ratingData[0].attributes.value;
+		// Set like/dislike icons depending on current song rating.
+		comRPC("GET", `rating/${playbackInfo.data?.playParams?.kind}/${playbackInfo.data?.playParams?.id}`).then((ratingData) => {
+
+			if (ratingData[0]?.attributes?.value === undefined) {
+				ratingData = [{attributes: {value: 0}}];
+				window.ratingCache = ratingData[0]?.attributes?.value ?? 0;
+			}
 
 			switch (ratingData[0].attributes.value) {
 				case 1:
-					forEach(window.contexts.likeAction, function (context) {
+					window.contexts.likeAction.forEach(function (context) {
 						setImage(context, 'actions/playback/assets/liked.png', 0);
 					});
-					forEach(window.contexts.dislikeAction, function (context) {
+					window.contexts.dislikeAction.forEach(function (context) {
 						setImage(context, 'actions/playback/assets/dislike.png', 0);
 					});
 					break;
 				case -1:
-					forEach(window.contexts.likeAction, function (context) {
+					window.contexts.likeAction.forEach(function (context) {
 						setImage(context, 'actions/playback/assets/like.png', 0);
 					});
-					forEach(window.contexts.dislikeAction, function (context) {
+					window.contexts.dislikeAction.forEach(function (context) {
 						setImage(context, 'actions/playback/assets/disliked.png', 0);
 					});
 					break;
 				case 0:
-					forEach(window.contexts.likeAction, function (context) {
+					window.contexts.likeAction.forEach(function (context) {
 						setImage(context, 'actions/playback/assets/like.png', 0);
 					});
-					forEach(window.contexts.dislikeAction, function (context) {
+					window.contexts.dislikeAction.forEach(function (context) {
 						setImage(context, 'actions/playback/assets/dislike.png', 0);
 					});
 					break;
 				}
-		})
-	} catch (error) {
-		console.debug("[DEBUG] [ERROR] An error occurred while processing the rating:", error);
+		});
 	}
+
+	// Set Action to use the correct icon
+	if (playbackInfo.data?.status === "playing") {
+        window.contexts.toggleAction.forEach(function (context) {
+		    setImage(context, 'actions/playback/assets/pause.png', 0);
+        })
+	}
+	else if (playbackInfo.data?.status === "paused") {
+        window.contexts.toggleAction.forEach(function (context) {
+		    setImage(context, 'actions/playback/assets/play.png', 0);
+        });
+	}
+
+	return;
 }
 
 // Playback Status event handling
 
 async function setPlaybackStatus(status) {
-	if (window.statusCache !== status.isPlaying) {
-		window.statusCache = status.isPlaying;
+	if (window.statusCache !== status) {
+		window.statusCache = status;
 		console.debug("[DEBUG] [Status] Status is different, updating.")
-		if (status.isPlaying) {
+		if (status) {
 			console.debug("[DEBUG] [Status] Setting as playing.")
-            forEach(window.contexts.toggleAction, function (context) {
+            window.contexts.toggleAction.forEach(function (context) {
 			    setImage(context, 'actions/playback/assets/pause.png', 0);
             });
-		} else if (!status.isPlaying) {
+		} else if (!status) {
 			console.debug("[DEBUG] [Status] Setting as paused.")
-            forEach(window.contexts.toggleAction, function (context) {
+            window.contexts.toggleAction.forEach(function (context) {
 			    setImage(context, 'actions/playback/assets/play.png', 0);
             });
 		}
@@ -210,8 +220,8 @@ async function setPlaybackStatus(status) {
 
 async function addToLibrary() {
 	if (!window.addedToLibrary) {
-		comRPC("GET", "addToLibrary");
-		forEach(window.contexts.addToLibraryAction, function (context) {
+		comRPC("GET", "addToLibrary", true);
+		window.contexts.addToLibraryAction.forEach(function (context) {
 			setImage(context, 'actions/playback/assets/check.png', 0);
 		});
 		window.addedToLibrary = true;
@@ -239,7 +249,7 @@ async function setVolume(direction) {
   		}
 
 		// Finally, set the volume.
-		comRPC("GET", "audio/"+newVolume);
+		comRPC("GET", "audio/"+newVolume, true);
 	});
 }
 
@@ -248,32 +258,32 @@ async function setVolume(direction) {
 
 async function setRating(rating) {
 	if (window.ratingCache !== rating) {
-		// Rating is different, update.
-		comRPC("PUT", `rating/${playbackInfo.info?.playParams?.kind}/${playbackInfo.info?.songId}/${rating}`);
+		console.debug("[DEBUG] [Status] Rating is different, updating.")
+		comRPC("PUT", `rating/${await window.kind}/${await window.id}/${rating}`, true);
 		switch (rating) {
 			case 1:
-				forEach(window.contexts.likeAction, function (context) {
+				window.contexts.likeAction.forEach(function (context) {
 					setImage(context, 'actions/playback/assets/liked.png', 0);
 				});
-				forEach(window.contexts.dislikeAction, function (context) {
+				window.contexts.dislikeAction.forEach(function (context) {
 					setImage(context, 'actions/playback/assets/dislike.png', 0);
 				});
 				window.ratingCache = 1;
 				break;
 			case -1:
-				forEach(window.contexts.likeAction, function (context) {
+				window.contexts.likeAction.forEach(function (context) {
 					setImage(context, 'actions/playback/assets/like.png', 0);
 				});
-				forEach(window.contexts.dislikeAction, function (context) {
+				window.contexts.dislikeAction.forEach(function (context) {
 					setImage(context, 'actions/playback/assets/disliked.png', 0);
 				});
 				window.ratingCache = -1;
 				break;
 			case 0:
-				forEach(window.contexts.likeAction, function (context) {
+				window.contexts.likeAction.forEach(function (context) {
 					setImage(context, 'actions/playback/assets/like.png', 0);
 				});
-				forEach(window.contexts.dislikeAction, function (context) {
+				window.contexts.dislikeAction.forEach(function (context) {
 					setImage(context, 'actions/playback/assets/dislike.png', 0);
 				});
 				window.ratingCache = 0;
@@ -281,12 +291,13 @@ async function setRating(rating) {
 		}
 	} else {
 		// Rating is the same reset to 0, assume the user wants to reset the rating.
-		forEach(window.contexts.likeAction, function (context) {
+		window.contexts.likeAction.forEach(function (context) {
 			setImage(context, 'actions/playback/assets/like.png', 0);
 		});
-		forEach(window.contexts.dislikeAction, function (context) {
+		window.contexts.dislikeAction.forEach(function (context) {
 			setImage(context, 'actions/playback/assets/dislike.png', 0);
 		});
+		comRPC("PUT", `rating/${await window.kind}/${await window.id}/${rating}`, true);
 		window.ratingCache = 0;
 	}
 }
@@ -340,52 +351,52 @@ window.contexts = {
 // Await the contexts and add them to the array.
 
 toggleAction.onWillAppear(({ context }) => {
-	if (window.contexts.toggleAction.indexOf(context) > -1) {
+	if (window.contexts.toggleAction.indexOf(context) === -1) {
 		window.contexts.toggleAction.push(context);
 	}
 });
 skipAction.onWillAppear(({ context }) => {
-	if (window.contexts.skipAction.indexOf(context) > -1) {
+	if (window.contexts.skipAction.indexOf(context) === -1) {
 		window.contexts.skipAction.push(context);
 	}
 });
 previousAction.onWillAppear(({ context }) => {
-	if (window.contexts.previousAction.indexOf(context) > -1) {
+	if (window.contexts.previousAction.indexOf(context) === -1) {
 		window.contexts.previousAction.push(context);
 	}
 });
 songNameAction.onWillAppear(({ context }) => {
-	if (window.contexts.songNameAction.indexOf(context) > -1) {
+	if (window.contexts.songNameAction.indexOf(context) === -1) {
 		window.contexts.songNameAction.push(context);
 	}
 });
 albumArtAction.onWillAppear(({ context }) => {
-	if (window.contexts.albumArtAction.indexOf(context) > -1) {
+	if (window.contexts.albumArtAction.indexOf(context) === -1) {
 		window.contexts.albumArtAction.push(context);
 	}
 });
 likeAction.onWillAppear(({ context }) => {
-	if (window.contexts.likeAction.indexOf(context) > -1) {
+	if (window.contexts.likeAction.indexOf(context) === -1) {
 		window.contexts.likeAction.push(context);
 	}
 });
 dislikeAction.onWillAppear(({ context }) => {
-	if (window.contexts.dislikeAction.indexOf(context) > -1) {
+	if (window.contexts.dislikeAction.indexOf(context) === -1) {
 		window.contexts.dislikeAction.push(context);
 	}
 });
 addToLibraryAction.onWillAppear(({ context }) => {
-	if (window.contexts.toggleLibraryAction.indexOf(context) > -1) {
-		window.contexts.toggleLibraryAction.push(context);
+	if (window.contexts.addToLibraryAction.indexOf(context) === -1) {
+		window.contexts.addToLibraryAction.push(context);
 	}
 });
 volumeUpAction.onWillAppear(({ context }) => {
-	if (window.contexts.volumeUpAction.indexOf(context) > -1) {
+	if (window.contexts.volumeUpAction.indexOf(context) === -1) {
 		window.contexts.volumeUpAction.push(context);
 	}
 });
 volumeDownAction.onWillAppear(({ context }) => {
-	if (window.contexts.volumeDownAction.indexOf(context) > -1) {
+	if (window.contexts.volumeDownAction.indexOf(context) === -1) {
 		window.contexts.volumeDownAction.push(context);
 	}
 });
@@ -444,7 +455,7 @@ volumeDownAction.onWillDisappear(({ context }) => {
 });
 
 // RPC Function for Key Events
-async function comRPC(method, request) {
+async function comRPC(method, request, noCheck) {
 	return fetch('http://localhost:10769/'+request, {
   		method: method,
   		headers: {
@@ -455,8 +466,9 @@ async function comRPC(method, request) {
   	.then(json => {
 		return json;
 	})
-  	.catch(error => console.debug("[DEBUG] [ERROR] An error occurred while processing the request:", error));
+  	.catch(error => {if (!noCheck) { console.debug("[DEBUG] [ERROR] An error occurred while processing the request:", error) }});
 }
+
 // Utility Functions
 function setImage(action, image, context) {
 	if (action !== null && image !== null && context !== null) {
